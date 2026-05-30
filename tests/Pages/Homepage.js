0ae680b9e0f202a335a9b25834f41f9e9b6e23a7
Sharmila B <sharmilaboopathi6@gmail.com>
2026-05-30 11:41:49 +0530

    Update Homepage.js

--- tests/Pages/Homepage.js
+++ tests/Pages/Homepage.js
@@ -1,6 +1,6 @@
 import { expect } from "@playwright/test";
 export class Homepage{
-
+//added new line
     constructor(page){
         this.page=page;
         this.successTitle=page.locator('//h1[text()="Logged In Successfully"]');
